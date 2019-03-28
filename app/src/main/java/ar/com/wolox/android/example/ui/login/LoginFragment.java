5561480d1db44508ba265b1ff108aeb9c4b6f9a9
diego alarcon <diego.alarcon@wolox.com.ar>
2019-03-28 12:39:24 -0300

    Added form validations

--- app/src/main/java/ar/com/wolox/android/example/ui/login/LoginFragment.java
+++ app/src/main/java/ar/com/wolox/android/example/ui/login/LoginFragment.java
@@ -1,12 +1,25 @@
 package ar.com.wolox.android.example.ui.login;
 
+import android.widget.Button;
+import android.widget.EditText;
+
 import ar.com.wolox.android.R;
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment;
+import butterknife.BindView;
 
 /**
  * Login fragment
  */
 public class LoginFragment extends WolmoFragment<LoginPresenter> implements ILoginView {
+
+    @BindView(R.id.vUserEmailInput)
+    EditText vUserEmailInput;
+
+    @BindView(R.id.vUserPasswordInput)
+    EditText vUserPasswordInput;
+
+    @BindView(R.id.vLoginButton)
+    Button vLoginButton;
 
     @Override
     public int layout() {
@@ -15,5 +28,50 @@ public class LoginFragment extends WolmoFragment<LoginPresenter> implements ILog
 
     @Override
     public void init() {
+        loadLocalLoginData();
+    }
+
+    @Override
+    public void setListeners() {
+        vLoginButton.setOnClickListener(v -> attemptToLogin());
+    }
+
+    private void attemptToLogin() {
+        String userEmail = vUserEmailInput.getText().toString();
+        String password = vUserPasswordInput.getText().toString();
+        getPresenter().validateForm(userEmail, password);
+    }
+
+    @Override
+    public void onEmptyEmail() {
+        vUserEmailInput.setError(getString(R.string.fragment_login_empty_email_error_message));
+    }
+
+    @Override
+    public void onEmptyPassword() {
+        vUserPasswordInput.setError(getString(R.string.fragment_login_empty_password_error_message));
+    }
+
+    @Override
+    public void onInvalidEmail() {
+        vUserEmailInput.setError(getString(R.string.fragment_login_invalid_email_error_message));
+    }
+
+    @Override
+    public void onFormIsValid() {
+        saveLocalLoginData();
+    }
+
+    @Override
+    public void onGetUserEmail(String userEmail) {
+        vUserEmailInput.setText(userEmail);
+    }
+
+    private void saveLocalLoginData() {
+        getPresenter().saveLocalLoginData(vUserEmailInput.getText().toString());
+    }
+
+    private void loadLocalLoginData() {
+        getPresenter().getLocalLoginData();
     }
 }
--- app/src/main/java/ar/com/wolox/android/example/ui/login/LoginPresenter.java
+++ app/src/main/java/ar/com/wolox/android/example/ui/login/LoginPresenter.java
@@ -1,12 +1,71 @@
 package ar.com.wolox.android.example.ui.login;
 
+import android.util.Patterns;
+
 import javax.inject.Inject;
 
 import ar.com.wolox.wolmo.core.presenter.BasePresenter;
 
-class LoginPresenter extends BasePresenter<ILoginView> {
+/**
+ * Login Presenter
+ */
+public class LoginPresenter extends BasePresenter<ILoginView> {
+
+    private LoginRepository mLoginRepository;
 
     @Inject
-    LoginPresenter() {
+    public LoginPresenter(LoginRepository loginRepository) {
+        this.mLoginRepository = loginRepository;
+    }
+
+    void saveLocalLoginData(String userEmail) {
+        mLoginRepository.saveLoginData(userEmail);
+    }
+
+    void getLocalLoginData() {
+        getUserEmail();
+    }
+
+    private void getUserEmail() {
+        getView().onGetUserEmail(mLoginRepository.getUserEmail());
+    }
+
+    void validateForm(String userEmail, String userPassword) {
+        if (validateUserEmail(userEmail) && validateUserPassword(userPassword)) {
+            getView().onFormIsValid();
+        }
+    }
+
+    private Boolean validateUserEmail(String userEmail) {
+        if (isEmptyUserEmail(userEmail)) {
+            getView().onEmptyEmail();
+            return false;
+        } else if (!isValidUserEmail(userEmail)) {
+            getView().onInvalidEmail();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private Boolean validateUserPassword(String userPassword) {
+        if (isEmptyUserPassword(userPassword)) {
+            getView().onEmptyPassword();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private Boolean isValidUserEmail(String userEmail) {
+        return Patterns.EMAIL_ADDRESS.matcher(userEmail).matches();
+    }
+
+    private Boolean isEmptyUserEmail(String userEmail) {
+        return userEmail.isEmpty();
+    }
+
+    private Boolean isEmptyUserPassword(String userPassword) {
+        return userPassword.isEmpty();
     }
 }
